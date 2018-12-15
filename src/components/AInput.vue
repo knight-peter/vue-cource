5bc5ab51d307dd5aa88e90d46bd52bb8daa3760c
徐子玉 <xuqipeter@qq.com>
2018-12-15 17:25:26 +0800

    +状态管理bus的使用

---
+++ src/components/AInput.vue
@@ -0,0 +1,17 @@
+<template>
+  <div><input type="text" @input="handleInput" :value="value" /></div>
+</template>
+
+<script>
+export default {
+  name: "AInput",
+  props: {
+    value: {
+      type: [String, Number],
+      default: ""
+    }
+  }
+};
+</script>
+
+<style scoped></style>
